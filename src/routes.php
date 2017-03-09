8d38900c44cda3258ab3ad0432fa9787504433f6
rushhourX <steffen.mueller27@gmail.com>
2017-03-09 17:07:04 +0100

    Adjust src for own App. DBO and Routes fpr Todos

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,22 @@
 <?php
 // Routes
 
+// get all todos
+    $app->get('/todos', function ($request, $response, $args) {
+         $sth = $this->db->prepare("SELECT * FROM tasks ORDER BY task");
+        $sth->execute();
+        $todos = $sth->fetchAll();
+        return $this->response->withJson($todos);
+    });
+// Retrieve todo with id
+    $app->get('/todo/[{id}]', function ($request, $response, $args) {
+         $sth = $this->db->prepare("SELECT * FROM tasks WHERE id=:id");
+        $sth->bindParam("id", $args['id']);
+        $sth->execute();
+        $todos = $sth->fetchObject();
+        return $this->response->withJson($todos);
+    });
+
 $app->get('/[{name}]', function ($request, $response, $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
--- src/settings.php
+++ src/settings.php
@@ -15,5 +15,12 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+        // Database connection settings
+        "db" => [
+            "host" => "localhost",
+            "dbname" => "slim_tasks",
+            "user" => "db-user",
+            "pass" => "password"
+        ],
     ],
 ];
